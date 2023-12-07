cf350428237bd10dc5890b762a09ef916ce374e2
Atlasx <cjdouglas96@gmail.com>
2023-12-06 18:53:49 -1000

    Initial Commit

---
+++ include/AsteroidsConfig.h
@@ -0,0 +1,7 @@
+#ifndef ASTEROIDSCONFIG_H
+#define ASTEROIDSCONFIG_H
+
+#define Asteroids_VERSION_MAJOR 0
+#define Asteroids_VERSION_MINOR 0
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "AsteroidsConfig.h"
+
+#include <cstdio>
+
+int main(int argc, char** argv)
+{
+	std::printf("Hello world\n");
+
+	std::printf("Asteroids Version %d.%d\n", Asteroids_VERSION_MAJOR, Asteroids_VERSION_MINOR);
+}
